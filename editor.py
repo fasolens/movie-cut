2c76432521c7bc000ec72d56c4d5dc995bb1d35f
Bartlomiej Zielinski <b.zielinski@radytek.com>
2018-02-26 15:20:07 +0100

    [SRC] Refactoring

--- editor.py
+++ editor.py
@@ -15,6 +15,9 @@ and two for closing
     One frame is from movie, the next one is just after movie
 """
 import movie
+import subprocess
+import os
+
 
 class Editor:
     def __init__(self, video: movie.Movie,
@@ -26,32 +29,107 @@ class Editor:
         self.opening = opening
         self.closing_movie = closing_movie
         self.closing = closing
-        print("Editor")
+        self.video = video
 
-    def find_frame(self):
+    def compare_images(self, sample_img: str, current_img: str) -> (int, str):
+        """Compare two images and return tuple of a comparison vector and path to second image
+
+        :param sample_img: path to first image
+        :type sample_img: str
+        :param current_img: path to second image
+        :type current_img: str
+        :return: (vector of comparison, path to current_imt)
+        :rtype: (int, str)
+        """
+        d = subprocess.run(['compare',
+                            '-metric',
+                            'MAE',
+                            sample_img,
+                            current_img,
+                            'null:',
+                            '2>&1'],
+                           stdout=subprocess.PIPE,
+                           stderr=subprocess.PIPE)
+        res = d.stderr.decode('utf-8')
+        res = res.split(" ")
+        return res[0], current_img
+
+    def get_frame_number_from_filename(self, fn: str) -> int:
+        """Converts filename to number of frame
+
+        :param fn: path to file
+        :type fn: str
+        :return: frame number
+        :rtype: int
+        """
+        fn = fn.split(os.sep)[-1]
+        fn = fn.split('.')[0]
+        num = int(fn)
+        return num
+
+    def find_frame(self,
+                   frames,  # list of frames
+                   sample_frame,  # path
+                   opening_offset=0,  # int
+                   closing_offset=0):  # int
+        FOUND = False
+        found_frames = []
+
+        for fn in frames:
+            file_to_compare = os.path.join('tmp', fn)
+            compare_result = self.compare_images(sample_frame, file_to_compare)
+            print(compare_result)
+
+    def find_opening_frame(self,
+                           frames,
+                           opening_offset):
+        self.find_frame(frames,
+                        self.opening,
+                        opening_offset=opening_offset)
+
+    def find_opening_movie_frame(self,
+                                 frames):
+        self.find_frame(frames,
+                        self.opening_movie)
+
+    def find_opening(self,
+                     frames,
+                     opening_offset):
+        if self.opening_movie:
+            self.find_opening_movie_frame(frames)
+        self.find_opening_frame(frames,
+                                opening_offset)
+
+    def find_closing_frame(self,
+                           frames,
+                           closing_offset):
+        self.find_frame(frames,
+                        self.closing,
+                        closing_offset=closing_offset)
+
+    def find_closing_movie_frame(self,
+                                 frames):
+        self.find_frame(frames,
+                        self.closing_movie)
+
+    def find_closing(self,
+                     frames,
+                     closing_offset):
+        if self.closing_movie:
+            self.find_closing_movie_frame(frames)
+        self.find_closing_frame(frames,
+                                closing_offset)
+
+    def cut(self):
         pass
 
-    def find_opening_frame(self):
-        self.find_frame()
-
-    def find_opening_movie_frame(self):
-        self.find_frame()
-
-    def find_opening(self):
-        if self.opening_movie:
-            self.find_opening_movie_frame()
-        self.find_opening_frame()
-
-    def find_closing_frame(self):
-        self.find_frame()
-
-    def find_closing_movie_frame(self):
-        self.find_frame()
-
-    def find_closing(self):
-        if self.closing_movie:
-            self.find_closing_movie_frame()
-        self.find_closing_frame()
-
-    def run(self):
-        self.find_opening()
+    def run(self,
+            opening_offset=None,
+            closing_offset=None):
+        frames = os.listdir('tmp')
+        frames.sort()
+        self.find_opening(frames,
+                          opening_offset)
+        self.find_closing(frames,
+                          closing_offset)
+        self.cut()
